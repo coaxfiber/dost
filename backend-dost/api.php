5d60e809e6bdbefaf6cb493fc0fb78f2a76b0cb6
elton bagne <coaxfiber05@gmail.com>
2019-05-03 17:19:32 +0800

    testing2

--- backend-dost/api.php
+++ backend-dost/api.php
@@ -12,7 +12,13 @@ error_reporting(E_ALL);
 include_once 'config/database.php';
 
 
-$possible_url = array("proposalinsert","proposallists", "proposaldelete", 'programinsert', 'getprojecttitles','projectadd','removeprojecttitle','removebudget','removebudget2','FundingAgency_List','addbudget','getbudget','addbudget2','getbudget2','proposaldone','login','getuserinfo','projectinsert','company_List','projectaddcoopagency','getcoopagency','projectdeletecoopagency');
+$possible_url = array("proposalinsert","proposallists", "proposaldelete", 'programinsert', 'getprojecttitles','projectadd','removeprojecttitle','removebudget','removebudget2','FundingAgency_List','addbudget','getbudget','addbudget2','getbudget2','proposaldone','login','getuserinfo','projectinsert','company_List','projectaddcoopagency','getcoopagency','projectdeletecoopagency',
+  'projectclassificationupdate',
+  'spProposal_ProjectDiscipline_Insert_Update',
+  'spProposal_ProjectPriorityAgenda_Insert_Update',
+  'spProposal_ProjectMOI_Insert_Update',
+  'spProposal_ProjectSector_Insert_Update',
+  'statuschange');
 
 $value = "An error has occurred";
 
@@ -20,6 +26,25 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
 {
   switch ($_GET["action"])
     {
+
+      case "statuschange":
+        $value = statuschange($_POST["proposalid"],$_POST["statusid"],$_POST["remarks"],$_POST["userid"],$_POST["type"]);
+        break;
+      case "spProposal_ProjectSector_Insert_Update":
+        $value = spProposal_ProjectSector_Insert_Update($_GET["projectid"],$_GET["cid"]);
+        break;
+      case "spProposal_ProjectMOI_Insert_Update":
+        $value = spProposal_ProjectMOI_Insert_Update($_GET["projectid"],$_GET["cid"]);
+        break;
+      case "spProposal_ProjectPriorityAgenda_Insert_Update":
+        $value = spProposal_ProjectPriorityAgenda_Insert_Update($_GET["projectid"],$_GET["cid"]);
+        break;
+      case "spProposal_ProjectDiscipline_Insert_Update":
+        $value = spProposal_ProjectDiscipline_Insert_Update($_GET["projectid"],$_GET["cid"]);
+        break;
+      case "projectclassificationupdate":
+        $value = projectclassificationupdate($_GET["projectid"],$_GET["cid"]);
+        break;
       case "proposalinsert":
         $value = insertProposal(
           $_POST["GeneralTitle"],
@@ -89,24 +114,21 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
         break;
       case "projectinsert":
         $value = projectinsert(
-          $_POST["programid"],
+          $_POST["id"],
           $_POST["title"],
           $_POST["duration"],
-          $_POST["station"],
-          $_POST["implementation"],
+          $_POST["rndstation"],
+          $_POST["siteofi"],
           $_POST["significance"],
           $_POST["objectives"],
           $_POST["literature"],
-          $_POST["basis"],
+          $_POST["sbasis"],
           $_POST["methodology"],
+          $_POST["majora"],
           $_POST["expectedoutput"],
           $_POST["targetb"],
-          $_POST["programid"],
-          $_POST["programid"],
-          $_POST["programid"],
-          $_POST["programid"],
-          $_POST["programid"],
-          $_POST["programid"]);
+          $_POST["start"],
+          $_POST["completion"]);
         break;
       case "getcoopagency":
         $value = getcoopagency($_GET["projectid"]);
@@ -120,6 +142,138 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
         $value = projectdeletecoopagency($_GET["id"]);
         break;
     }
+}
+
+function statuschange($proposalid,$statusid,$remarks,$userid,$type)
+{
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $stmt = $db->prepare("exec spProposal_Status_Insert
+    @Proposal_id = $proposalid,
+    @StatusID = $statusid,
+    @Remarks = '$remarks',
+    @UserId = $userid,
+    @Type = $type");  
+  $stmt->execute();
+  $app_list = array(
+              "ok" =>"ok"
+               );
+  return  $app_list;
+}
+function spProposal_ProjectSector_Insert_Update($pid,$cid)
+{
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $stmt = $db->prepare("exec spProposal_ProjectSector_Insert_Update
+    @Project_id = $pid,
+    @ProjectSector_id = $cid");  
+  $stmt->execute();
+  $app_list = array(
+              "ok" =>"ok"
+               );
+  return  $app_list;
+}
+function spProposal_ProjectMOI_Insert_Update($pid,$cid)
+{
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $stmt = $db->prepare("exec spProposal_ProjectMOI_Insert_Update
+    @Project_id = $pid,
+    @MOI_ID = $cid");  
+  $stmt->execute();
+  $app_list = array(
+              "ok" =>"ok"
+               );
+  return  $app_list;
+}
+function spProposal_ProjectPriorityAgenda_Insert_Update($pid,$cid)
+{
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $stmt = $db->prepare("exec spProposal_ProjectPriorityAgenda_Insert_Update
+    @Project_id = $pid,
+    @PriorityAgenda_id = $cid");  
+  $stmt->execute();
+  $app_list = array(
+              "ok" =>"ok"
+               );
+  return  $app_list;
+}
+function projectclassificationupdate($pid,$cid)
+{
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $stmt = $db->prepare("exec spProposal_ProjectClassification_Insert_Update
+    @Project_id = $pid,
+    @ProjectClassification_id = $cid");  
+  $stmt->execute();
+  $app_list = array(
+              "ok" =>"ok"
+               );
+  return  $app_list;
+}
+function spProposal_ProjectDiscipline_Insert_Update($pid,$cid)
+{
+  $database = new Database();
+  $db = $database->getConnection();
+
+  $stmt = $db->prepare("exec spProposal_ProjectDiscipline_Insert_Update
+    @Project_id = $pid,
+    @ProjectDiscipline_id = $cid");  
+  $stmt->execute();
+  $app_list = array(
+              "ok" =>"ok"
+               );
+  return  $app_list;
+}
+function projectinsert($id,$title,$duration,$rndstation,$siteofi,$significance,$objectives,$literature,$sbasis,$methodology,$majora,$expectedoutput,$targetb,$start,$completion){
+
+ $database = new Database();
+  $db = $database->getConnection();
+
+  $title=htmlspecialchars(strip_tags($title));
+  $duration=htmlspecialchars(strip_tags($duration));
+  $rndstation=htmlspecialchars(strip_tags($rndstation));
+  $siteofi=htmlspecialchars(strip_tags($siteofi));
+  $significance=htmlspecialchars(strip_tags($significance));
+  $objectives=htmlspecialchars(strip_tags($objectives));
+  $literature=htmlspecialchars(strip_tags($literature));
+  $sbasis=htmlspecialchars(strip_tags($sbasis));
+  $majora=addslashes($majora);
+  $methodology=htmlspecialchars(strip_tags($methodology));
+  $expectedoutput=htmlspecialchars(strip_tags($expectedoutput));
+  $targetb=htmlspecialchars(strip_tags($targetb));
+
+  //    @SiteOfImplementation_PSGC = '$Address_PSGC',
+
+  $stmt = $db->prepare("exec spProposal_Project_Update
+    @id = $id,
+    @ProjectTitle = '$title',
+    @Duration = $duration,
+    @RAndDStation = '$rndstation',
+    @Significance = '$significance',
+    @Objectives = '$objectives',
+    @ReviewOfLiterature = '$literature',
+    @ScientificBasis = '$sbasis',
+    @Methodology = '$methodology',
+    @ExpectedOutput = '$expectedoutput',
+    @TargetBeneficiaries = '$targetb',
+    @WorkPlan_attachment = :majora,
+    @PlannedStartDate = '$start',
+    @PlannedCompletionDate = '$completion'");
+  $stmt->bindParam(':majora', $majora);  
+  $stmt->execute();
+  $proposalid = $stmt ->fetch();
+  $app_list = array(
+              "id" => $id );
+    
+
+  return  $app_list;
 }
 
 function projectdeletecoopagency($id)
@@ -246,7 +400,32 @@ $db = $database->getConnection();
   $stmt->execute();
   
   $x=0;
+
+  $stmt = $db->prepare("exec spProposal_List
+    @UserID = '$userid'");  
+  $stmt->execute();
+
+
 while ($proposal = $stmt ->fetch()) {
+  
+$ents=$proposal[0];
+   $stmt2 = $db->prepare("exec spProposal_Status_List
+    @Proposal_id = $ents");  
+  $stmt2->execute();
+  $y=0;
+  while ($status = $stmt2 ->fetch()) {
+     $stat[$y] = array(
+                "id" => $status[0],
+                "name" => $status[1],
+                "remarks" => $status[2],
+                "createdby" => $status[3],
+                "datecreated" => $status[4],
+                "updatedby" => $status[5],
+                "dateupdated" => $status[6],
+                "type" => $status[7]
+                 );
+     $y++;
+  }
    $app_list[$x] = array(
               "id" => $proposal[0],
               "GeneralTitle" => $proposal[1],
@@ -257,7 +436,10 @@ while ($proposal = $stmt ->fetch()) {
               "Fax" => $proposal[6],
               "Email" => $proposal[7],
               "FundingAgency" => $proposal[8],
+              "status" => $stat
                );
+   
+
    $x++;
 }
   return  $app_list;
@@ -432,12 +614,12 @@ while ($row = $stmt ->fetch()) {
               "pdstation" =>  $row[3],
               "siteoi" =>  $row[4], 
               "significance" => $row[5], 
-              "objectives" => $row[7], 
-              "revlit" => $row[9], 
-              "sbasis" =>  $row[17], 
-              "methodology" =>  $row[13] , 
-              "eoutput" =>  $row[15],
-              "targetb" =>  $row[17]  );
+              "objectives" => $row[8], 
+              "revlit" => $row[11], 
+              "sbasis" =>  $row[14], 
+              "methodology" =>  $row[17] , 
+              "eoutput" =>  $row[20],
+              "targetb" =>  $row[23]  );
             $x++;
         
 }
@@ -572,5 +754,35 @@ function getuserinfo($id)
 
 
 //return JSON array
+if ($value == "An error has occurred") {
+$filename="sable";
+  $database = new Database();
+$db = $database->getConnection();
+$programid=1128;
+  $stmt = $db->prepare("exec spProposal_Project_List
+    @Program_id = $programid");  
+  $stmt->execute();
+  $app_list = array(
+              "id" => null);
+  $app_list = array(
+               0 => $app_list);
+  $x=0;
+while ($row = $stmt ->fetch()) {
+    $str =  $row[26];
+        
+}
+
+
+//header('Content-Type:application/pdf');
+header('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Transfer-Encoding: binary');
+header('Content-Disposition: attachment; filename=my.xlsx');
+ob_clean();
+flush();
+echo base64_decode($str);
+
+exit();
+
+}else
 exit(json_encode($value));
 ?>
